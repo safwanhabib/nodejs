cee644bcaaf7c43e50a46c9bad284e2274b159c8
safwanhabib <saafwanhabib@gmail.com>
2015-05-26 22:01:59 +0500

    comm

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.post('/', function(req, res) {
+  res.send(req.body.name);
+});
 
 module.exports = router;
